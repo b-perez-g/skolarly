4422b88e8a72666184ee17b4cf6c1b9400cdbd4f
b-perez-g <bperezg.informatica@gmail.com>
2023-11-08 23:12:35 -0300

    Rutas en app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,23 @@ app.get('/', (req, res)=>{
 })
 
 //Rutas
+app.use('/api/actividades', require('.routes/actividad'))
+app.use('/api/anotaciones', require('.routes/anotacion'))
+app.use('/api/archivos-adjuntos', require('.routes/archivoadjunto'))
+app.use('/api/asignaturas', require('.routes/asignatura'))
+app.use('/api/asistencia', require('.routes/asistencia'))
+app.use('/api/autorizaciones', require('.routes/autorizacion'))
+app.use('/api/bloques-horarios', require('.routes/bloqueHorario'))
+app.use('/api/calificaciones', require('.routes/calificacion'))
+app.use('/api/capacitaciones', require('.routes/capacitacion'))
+app.use('/api/casos', require('.routes/caso'))
+app.use('/api/chats', require('.routes/chat'))
+app.use('/api/comunicaciones', require('.routes/comunicacion'))
+app.use('/api/cursos', require('.routes/curso'))
+app.use('/api/escuelas', require('.routes/escuela'))
+app.use('/api/medallas', require('.routes/medalla'))
+app.use('/api/medallas-apoderados', require('.routes/medallaapoderado'))
+app.use('/api/mensajes', require('.routes/mensaje'))
 app.use('/api/usuarios', require('./routes/usuario'))
 
 module.exports = app;
